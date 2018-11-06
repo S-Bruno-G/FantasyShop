cb9e39aef131caac71265ae7521da26887238ba0
Santiago Bruno <sbrunogonzalez@csumb.edu>
2018-11-06 04:08:19 +0000

    Added some CSS and a navigatin menu

--- index.php
+++ index.php
@@ -6,19 +6,39 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Fantasy Shop</title>
+        <title>Fantasy Shop</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel='stylesheet' href='css/styles.css' type='text/css' />
     </head>
     <body>
-        <h1> Fantasy Shop </h1>
-        <h2> Product Search </h2>
-        
+        <!-- Bootstrap Navagation Bar -->
+            <nav class='navbar navbar-default - navbar-fixed-top'>
+                <div class='container-fluid'>
+                    <div class='navbar-header'>
+                        <a class='navbar-brand' href='index.php'>Fantasy Shop</a>
+                    </div>
+                    <ul class='nav navbar-nav'>
+                        <li><a href='index.php'>Home</a></li>
+                        <li><a href='scart.php'>
+                        <span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'>
+                        </span> Cart: </a></li>
+                    </ul>
+                </div>
+            </nav>
+            <br /> <br /> <br />
+            
+            <img src="https://fontmeme.com/permalink/181105/4a00f79cfe7a04a29bbd1289d37dd1ca.png" alt="dragon-ball-z-font" border="0">
         <form>
             Product: <input type="text" name="productName" placeholder="Product keyword" /> <br />
             
             Category: 
             <select name="category">
-               <option value=""> Select one </option>  
+               <option value=""> -Select one- </option>  
                <?=displayCategories()?>
             </select>
             <br>
@@ -31,12 +51,12 @@
                 <?php if ($_GET['orderBy'] == "ASC") {
                 //echo "checked";
                 } ?>
-                /> ASC <br>
+                /> low-high <br>
               <input type="radio" name="orderBy" value="high-low"
                 <?php if ($_GET['orderBy'] == "DECS") {
                 //echo "checked";
                 } ?>
-                /> DECS <br>
+                /> high-low <br>
             <br>
             
             <input type="submit" name="submit" value="Search!"/>
--- purchaseHistory.php
+++ purchaseHistory.php
@@ -23,17 +23,20 @@ function displayProductInfo(){
         
     }
     
+    echo "<center>";
     echo "<table>";
     echo "<tr>";
-    echo "<th>Quantity</th><th>Unit Price</th><th> Purchase Date</th>";
+    echo "<th>Description-</th><th>Quantity-</th><th>Unit Price-</th><th>Purchase Date</th>";
     foreach ($records as $record) {
-        echo "<tr>";    
-        echo "<td>" . $record[quantity] . "</td>";
-        echo "<td>" . $record[unitPrice] . "</td>";
-        echo "<td>" . $record[purchaseDate] . "</td>";
+        echo "<tr>";
+        echo "<td>" . $record[productDescription] . "</td>";
+        echo "<td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $record[quantity] . "</td>";
+        echo "<td>&nbsp&nbsp&nbsp $" . $record[unitPrice] . "</td>";
+        echo "<td>&nbsp&nbsp&nbsp" . $record[purchaseDate] . "</td>";
         echo "</tr>";  
     }
     echo "</table>";
+    echo "</center>";
     
     //print_r($records);
     
@@ -47,11 +50,33 @@ function displayProductInfo(){
 <html>
     <head>
         <title> Product Purchase History </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel='stylesheet' href='css/styles.css' type='text/css' />
     </head>
     <body>
+        <!-- Bootstrap Navagation Bar -->
+            <nav class='navbar navbar-default - navbar-fixed-top'>
+                <div class='container-fluid'>
+                    <div class='navbar-header'>
+                        <a class='navbar-brand' href='index.php'>Fantasy Shop</a>
+                    </div>
+                    <ul class='nav navbar-nav'>
+                        <li><a href='index.php'>Home</a></li>
+                        <li><a href='scart.php'>
+                        <span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'>
+                        </span> Cart: </a></li>
+                    </ul>
+                </div>
+            </nav>
+            <br /> <br /> <br />
+    </head>
 
         <h2>Product Purchase History</h2>
-        
         <?=displayProductInfo()?>
         
     </body>
